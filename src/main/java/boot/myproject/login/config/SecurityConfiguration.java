d334e4eed86b9b780cc20cebede7ebae72d6569f
K1mSH <124352219+K1mSH@users.noreply.github.com>
2023-06-30 15:22:57 +0900

    title test to PR

--- src/main/java/boot/myproject/login/config/SecurityConfiguration.java
+++ src/main/java/boot/myproject/login/config/SecurityConfiguration.java
@@ -4,12 +4,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.ldap.EmbeddedLdapServerContextSourceFactoryBean;
-import org.springframework.security.config.ldap.LdapBindAuthenticationManagerFactory;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
@@ -45,15 +43,15 @@ public class SecurityConfiguration {
         contextSourceFactoryBean.setPort(0);
         return contextSourceFactoryBean;
     }
-    @Bean
-    AuthenticationManager ldapAuthenticationManager(
-            BaseLdapPathContextSource contextSource) {
-        LdapBindAuthenticationManagerFactory factory =
-                new LdapBindAuthenticationManagerFactory(contextSource);
-        factory.setUserDnPatterns("uid={0},ou=people");
-        factory.setUserDetailsContextMapper(new PersonContextMapper());
-        return factory.createAuthenticationManager();
-    }
+//    @Bean
+//    AuthenticationManager ldapAuthenticationManager(
+//            BaseLdapPathContextSource contextSource) {
+//        LdapBindAuthenticationManagerFactory factory =
+//                new LdapBindAuthenticationManagerFactory(contextSource);
+//        factory.setUserDnPatterns("uid={0},ou=people");
+//        factory.setUserDetailsContextMapper(new PersonContextMapper());
+//        return factory.createAuthenticationManager();
+//    }
 
     //JDBC Authentication
     @Bean
