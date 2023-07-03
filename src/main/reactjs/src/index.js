e5b82feff95e30a1e464db00ffca71495c8f7b0f
K1mSH <124352219+K1mSH@users.noreply.github.com>
2023-07-03 11:01:08 +0900

    test 중: root 공백 제거

--- src/main/reactjs/src/index.js
+++ src/main/reactjs/src/index.js
@@ -7,7 +7,7 @@ import Root from './Root';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Root />
+    <Root/>
   </React.StrictMode>
 );
 
