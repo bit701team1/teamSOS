4254c991e7ac05f49c20cc24e0d47ef1c9a0a2d9
K1mSH <124352219+K1mSH@users.noreply.github.com>
2023-07-03 11:32:20 +0900

    배포 확인

--- src/main/reactjs/src/RouteMain.js
+++ src/main/reactjs/src/RouteMain.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
-import GitTestHome from './GitTestHome';
+
 import errorimg from './image/ERR404.png'
+import GitTestHome from './com/GitTestHome';
 
 function RouteMain(props) {
     return (
         <div>
+            <h1>ㅏ하핳하하</h1>
             <Routes>
                 <Route path='/' element={GitTestHome}/>
                 <Route path='*' element={
