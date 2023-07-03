ab0510cee30e07649179a57a4bb88010aee0e83c
K1mSH <124352219+K1mSH@users.noreply.github.com>
2023-07-03 11:38:43 +0900

    react test

--- src/main/reactjs/src/RouteMain.js
+++ src/main/reactjs/src/RouteMain.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 
-import errorimg from './image/ERR404.png'
+import errorimg from './image/ERR404.png';
 import GitTestHome from './com/GitTestHome';
 
 function RouteMain(props) {
     return (
         <div>
-            <h1>ㅏ하핳하하</h1>
             <Routes>
-                <Route path='/' element={GitTestHome}/>
+                <Route path='/' element={<GitTestHome/>}/>
                 <Route path='*' element={
                     <div>
                         <h1>잘못된 URL 주소입니다</h1>
