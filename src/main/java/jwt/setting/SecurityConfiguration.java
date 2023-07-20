39e3ac0a0ecc04b27b5be998c7e89f9817ebb9a4
DonghoKim33 <124513071+DonghoKim33@users.noreply.github.com>
2023-07-20 15:19:48 +0900

    추가 : 중복입찰 방지(check-duplicate) 기능 추가 수정 : BidDto 추가 ProductController,Service에 check-duplicate 추가  SecurityConfiguration에 bid/** 추가

--- src/main/java/jwt/setting/SecurityConfiguration.java
+++ src/main/java/jwt/setting/SecurityConfiguration.java
@@ -8,6 +8,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -33,6 +34,15 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     private final UserMapper userMapper;
     private final TokenMapper tokenMapper;
 
+//    @Override
+//    public void configure(WebSecurity web) {
+//        web.ignoring()
+//                .antMatchers(
+//                        "/h2-console/**"
+//                        ,"/favicon.ico"
+//                        ,"/error"
+//                );
+//    }
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         System.out.println("HttpSecurity 진입");
@@ -75,6 +85,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .antMatchers("/livestream/**").permitAll()
                 .antMatchers("/payment/**").permitAll()
                 .antMatchers("/product/**").permitAll()
+                .antMatchers("/bid/**").permitAll()
 
                 //.antMatchers("/user/rejection").hasAnyAuthority("ROLE_USER", "ROLE_ADMIN")
                 //.antMatchers("/trade/**").hasAnyAuthority("ROLE_USER", "ROLE_ADMIN")
@@ -110,19 +121,17 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     }
 
     // Spring security룰을 무시하게 하는 url규칙
-//	@Override
-//	public void configure(WebSecurity web) {
-//		System.out.println("web security");
-//		web.ignoring()
-//		.antMatchers("/h2-console/**", "/favicon.ico")
-//		.antMatchers("/v2/api-docs", "/swagger-resources/**", "/swagger-ui.html", "/webjars/**", "/swagger/**");
-//		//              .antMatchers("/resources/**")
-//		//              .antMatchers("/css/**")
-//		//              .antMatchers("/vendor/**")
-//		//              .antMatchers("/js/**")
-//		//              .antMatchers("/favicon*/**")
-//		//              .antMatchers("/img/**")
-//	}
+    @Override
+    public void configure(WebSecurity web) {
+        System.out.println("web security");
+        web.ignoring()
+                .antMatchers("/h2-console/**", "/favicon.ico")
+                .antMatchers("/static/**","/main/**","/manifest/**","/resources/**","/css/**","/favicon*/**")
+                .antMatchers("/v2/api-docs", "/swagger-resources/**", "/swagger-ui.html", "/webjars/**", "/swagger/**");
+        //              .antMatchers("/vendor/**")
+        //              .antMatchers("/js/**")
+        //              .antMatchers("/img/**")
+    }
 
     //비밀번호 암호화를 위한 Encoder 설정
     @Bean
