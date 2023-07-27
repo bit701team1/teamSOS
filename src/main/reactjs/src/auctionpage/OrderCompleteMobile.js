e6a2d79cfc3bad1dd22c48fde347fac09d35490f
DonghoKim33 <124513071+DonghoKim33@users.noreply.github.com>
2023-07-27 12:53:04 +0900

    수정 : RouteMain 에서 ordercompletemobile 수정, OrderCompleteMobile.js 수정
    - RouteMain에서 param값 추가
    - OrderCompleteMobile에 useParams로 변경

--- src/main/reactjs/src/auctionpage/OrderCompleteMobile.js
+++ src/main/reactjs/src/auctionpage/OrderCompleteMobile.js
@@ -1,17 +1,19 @@
 import React, { useEffect, useState } from 'react';
 import axios from "axios";
+import { useParams } from "react-router-dom";
 
-function OrderCompleteMobile(props) {
+function OrderCompleteMobile() {
     const [result, setResult] = useState('');
 
-    useEffect(() => {
-        // URL 파라미터로 전달받은 값을 가져옵니다.
-        const { imp_uid, merchant_uid, imp_success } = props.match.params;
+    const { imp_uid, merchant_uid, imp_success } = useParams();
 
+    useEffect(() => {
         axios.get("/payment/getcompleteresult", {
-            imp_uid: imp_uid,
-            merchant_uid: merchant_uid,
-            imp_success: imp_success
+            params: {
+                imp_uid: imp_uid,
+                merchant_uid: merchant_uid,
+                imp_success: imp_success
+            }
         })
             .then(response => {
                 // 성공적으로 처리된 경우, response에 결제 정보가 담겨 있을 것입니다.
@@ -24,7 +26,7 @@ function OrderCompleteMobile(props) {
                 console.error(error);
                 // 에러 처리를 하시거나 필요한 작업을 수행하시면 됩니다.
             });
-    }, [props.match.params]); // URL 파라미터가 변경될 때마다 useEffect를 실행합니다.
+    }, [imp_uid, merchant_uid, imp_success]); // URL 파라미터가 변경될 때마다 useEffect를 실행합니다.
 
     return (
         <div>
