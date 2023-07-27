a89e228aae9349898764707ba4014fe4a5c84147
DonghoKim33 <124513071+DonghoKim33@users.noreply.github.com>
2023-07-28 00:48:46 +0900

    테스트

--- src/main/reactjs/src/auctionpage/OrderCompleteMobile.js
+++ src/main/reactjs/src/auctionpage/OrderCompleteMobile.js
@@ -25,7 +25,8 @@ function OrderCompleteMobile() {
             })
             .then((response) => {
                 // 성공적으로 처리된 경우, response에 결제 정보가 담겨 있을 것입니다.
-                console.log(response.data);
+                console.log("amount>"+response.data.response.amount);
+                console.log("imp_uid>"+response.data.response.imp_uid);
 
                 const { amount, imp_uid } = response.data;
 
@@ -45,8 +46,8 @@ function OrderCompleteMobile() {
                         .then((response) => {
                             console.log('결제 정보가 데이터베이스에 저장되었습니다.');
                             console.log(response.data);
-                            console.log("amount>"+response.data.amount+"&requestAmount>"+requestAmount);
-                            console.log("imp_uid>"+response.data.imp_uid+"&requsetImpUid>"+requsetImpUid);
+                            console.log("amount>"+response.data.response.amount+"&requestAmount>"+requestAmount);
+                            console.log("imp_uid>"+response.data.response.imp_uid+"&requsetImpUid>"+requsetImpUid);
                             setResult(true); // 결제가 성공한 경우 result 값을 true로 설정
                         })
                         .catch((error) => {
@@ -55,10 +56,11 @@ function OrderCompleteMobile() {
                         });
                 } else {
                     // 결제 정보가 일치하지 않는 경우
-                    console.log('결제 정보가 맞지 않습니다.')
-                    console.log(response.data);
-                    console.log("amount>"+response.data.amount+"&requestAmount>"+requestAmount);
-                    console.log("imp_uid>"+response.data.imp_uid+"&requsetImpUid>"+requsetImpUid);
+                    console.log('결제 정보가 맞지 않습니다.');
+                    console.log("amount>"+response.data.response.amount);
+                    console.log("imp_uid>"+response.data.response.imp_uid);
+                    console.log("amount>"+response.data.response.amount+"&requestAmount>"+requestAmount);
+                    console.log("imp_uid>"+response.data.response.imp_uid+"&requsetImpUid>"+requsetImpUid);
                     setResult(false); // 결제가 실패한 경우 result 값을 false로 설정
                 }
             })
