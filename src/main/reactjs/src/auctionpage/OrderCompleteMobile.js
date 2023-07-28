387a9a93c85db0318e1d8e47b4af606e894329ba
DonghoKim33 <124513071+DonghoKim33@users.noreply.github.com>
2023-07-28 11:46:05 +0900

    결제확인창 변수값 수정

--- src/main/reactjs/src/auctionpage/OrderCompleteMobile.js
+++ src/main/reactjs/src/auctionpage/OrderCompleteMobile.js
@@ -149,19 +149,19 @@ function OrderCompleteMobile() {
                 <div className="D_payment_container">
                     <div className="D_payment_item">
                         <label>상품명</label>
-                        <span>{paymentData.productName}</span>
+                        <span>{paymentData.data.productName}</span>
                     </div>
                     <div className="D_payment_item">
                         <label>거래번호</label>
-                        <span>{paymentData.merchant_uid}</span>
+                        <span>{paymentData.data.merchant_uid}</span>
                     </div>
                     <div className="D_payment_item">
                         <label>구매자명</label>
-                        <span>{paymentData.user_name}</span>
+                        <span>{paymentData.data.user_name}</span>
                     </div>
                     <div className="D_payment_item">
                         <label>결제금액</label>
-                        <span>{paymentData.amount}원</span>
+                        <span>{paymentData.data.amount}원</span>
                     </div>
                 </div>
                 <a href="/" className="D_button">결제 확인</a>
